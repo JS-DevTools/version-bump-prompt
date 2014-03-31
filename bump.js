73eabd32cb16b0c1325f81fe0b58ec68c71326c1
Avenger7x <avenger7x13@gmail.com>
2014-03-31 19:02:40 +0300

    cleanup

--- bump.js
+++ bump.js
@@ -2,55 +2,28 @@
 'use strict';
 
 var multiline = require('multiline'),
-	nopt = require('nopt'),
 	program = require('commander'),
 	bump = require('./index').bump;
 
-function getType(options){
-	var type;
-
-	type = ['major', 'minor', 'patch'].filter(function(type){
-		return options[type];
-	});
-
-	if(type.length){
-		return type[0];
-	}
+function init(type, program){
+	console.log(type, program);
+	bump(type);
 }
-
-function init(program){
-	var type;
-
-	type = getType(program);
-
-	console.log(type);
-	// return type && bump(type);
-}
-/*
-var options = nopt({
-	help: Boolean,
-	version: Boolean,
-	patch: Boolean,
-	minor: Boolean,
-	major: Boolean
-}, {
-	h: '--help',
-	v: '--version'
-});*/
-
-/*if(options.argv.original.length){
-	init(options);
-}else{
-	help();
-}
-*/
 
 program
 	.version(require('./package').version)
 	.usage('[options]')
-	.option('--patch', 'Increase patch version')
-	.option('--minor', 'Increase minor version')
-	.option('--major', 'Increase major version');
+	.option('--no-tags', 'Do not create git tag');
+
+['patch', 'minor', 'major'].forEach(function(type){
+	program.option('--' + type, 'Increase ' + type + ' version');
+
+	program.on(type, function(){
+		setTimeout(function(){
+			init(type, program);
+		}, 0);
+	});
+});
 
 program.on('--help', function(){
 	console.log(multiline(function(){/*
@@ -64,9 +37,7 @@ program.on('--help', function(){
 
 program.parse(process.argv);
 
-if(program.rawArgs.length > 2){
-	init(program);
-}else{
+if(program.rawArgs.length < 3){
 	program.help();
 }
 
