8ef793d33dc00709f6f2bebdff490f411d6e42fb
Avenger7x <avenger7x13@gmail.com>
2014-03-31 19:43:52 +0300

    use promises

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var semver = require('semver'),
-	exec = require('child_process').exec,
+	exec = require('child-process-promise').exec,
 	pkg = process.cwd() + '/package.json',
 	fs = require('fs'),
 	indent = require('detect-indent');
+
+function logError(err){
+	console.log(err.stderr);
+}
 
 exports.bump = function(type, options){
 	var current = require(pkg);
@@ -14,18 +18,19 @@ exports.bump = function(type, options){
 	fs.writeFileSync(pkg, JSON.stringify(current, null, usedIndent));
 
 	if(options.tags){
-		exec('git commit package.json -m "release v' + current.version + '"', function(err, stdout, stderr){
-			if(err){
-				console.log('commit', stderr);
-			}else{
-				exec('git tag v' + current.version, function(err, stdout, stderr){
-					if(err){
-						console.log(err);
-					}else{
-						console.log(current.version);
-					}
-				});
-			}
-		});
+		exec('git commit package.json -m "release v' + current.version + '"')
+			.then(function(out){
+				console.log(out.stdout);
+				return exec('git tag v' + current.version)
+			}, logError)
+
+			.then(function(out){
+				console.log(out.stdout, current.version);
+				return options.push && exec('git push && git push --tags');
+			}, logError)
+
+			.then(function(out){
+				console.log(out && out.stdout);
+			}, logError);
 	}
 };
