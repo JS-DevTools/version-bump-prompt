426c393cc95916863cea14df44521050d3441d86
Avenger7x <avenger7x13@gmail.com>
2014-03-31 16:27:13 +0300

    simple bump: only update version in package

--- cli.js
+++ cli.js
@@ -1,25 +1,57 @@
+#!/usr/bin/env node
+'use strict';
+
 var multiline = require('multiline');
 
 var nopt = require('nopt');
 
-var bump = require('./index');
+var bump = require('./index').bump;
 
-var help = multiline.stripIndent(function(){/*
-	Update package version and create tag
+function help(){
+	console.log(multiline.stripIndent(function(){/*
+		Update package version and create tag
 
-	Usage
-	  bump <type>
+		Usage
+		  bump <type>
 
-	Example
-	  bump --patch
-	  bump --minor
-	  bump --major
-*/});
+		Example
+		  bump --patch
+		  bump --minor
+		  bump --major
+	*/}));
+}
 
+function init(options){
+	if(options.help){
+		return help();
+	}
+
+	if(options.version){
+		return console.log(require('./package').version);
+	}
+
+	var type = ['major', 'minor', 'patch'].filter(function(type){
+		return options[type];
+	});
+
+	if(type.length){
+		return bump(type[0]);
+	}
+}
 
 var options = nopt({
 	help: Boolean,
+	version: Boolean,
 	patch: Boolean,
 	minor: Boolean,
 	major: Boolean
+}, {
+	h: '--help',
+	v: '--version'
 });
+
+if(options.argv.original.length){
+	init(options);
+}else{
+	help();
+}
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var semver = require('semver');
+var semver = require('semver'),
+	exec = require('child_process').exec,
+	pkg = process.cwd() + '/package.json',
+	fs = require('fs'),
+	indent = require('detect-indent');
 
-var exec = require('child_process').exec;
+exports.bump = function(type){
+	var current = require(pkg);
+
+	current.version = semver.inc(current.version, type);
+
+	var usedIndent = indent(fs.readFileSync(pkg, 'utf8')) || '  ';
+
+	fs.writeFileSync(pkg, JSON.stringify(current, null, usedIndent));
+};
