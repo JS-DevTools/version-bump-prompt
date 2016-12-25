1f7defc25b47a659c1b6690fa2061c68de7db762
James Messinger <james.messinger@gmail.com>
2016-12-25 17:14:42 -0600

    updated to ES6 syntax

--- bin/bump.js
+++ bin/bump.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 'use strict';
 
-var program  = require('commander'),
-    inquirer = require('inquirer'),
-    chalk    = require('chalk'),
-    api      = require('../');
+const program = require('commander');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const api = require('../');
 
 program
   .version(require('../package').version)
@@ -22,7 +22,7 @@ program
   .option('--push', 'Push the Git commit')
   .option('--all', 'Commit/tag/push ALL pending files, not just the ones changed by bump')
   .option('--grep <filespec...>', 'Files and/or globs to do a text-replace of the old version number with the new one')
-  .on('--help', function() {
+  .on('--help', () => {
     console.log(
       '  Examples:\n' +
       '\n' +
@@ -39,7 +39,7 @@ if (program.rawArgs.length < 3) {
   program.help();
 }
 else {
-  var options = program;
+  let options = program;
 
   if (options.grep && program.args) {
     // If multiple --grep files are specified, then they are parsed as separate args
@@ -51,18 +51,18 @@ else {
     options.commit = true;
   }
 
-  var manifests = api.manifests();
+  let manifests = api.manifests();
   bumpManifests(manifests, options)
-    .then(function() {
+    .then(() => {
       api.grep(manifests, options);
-      manifests.forEach(function (manifest) {
+      manifests.forEach((manifest) => {
         api.runNpmScriptIfExists(manifest, 'version');
       });
     })
-    .then(function() {
+    .then(() => {
       api.git(manifests, options);
     })
-    .catch(function(err) {
+    .catch((err) => {
       console.error(chalk.red(err.message));
       process.exit(err.status || 1);
     });
@@ -75,13 +75,13 @@ else {
  * @param {object} options - CLI options
  * @returns {Promise}
  */
-function bumpManifests(manifests, options) {
-  var i = 0;
+function bumpManifests (manifests, options) {
+  let i = 0;
 
   return bumpNext('patch');
 
-  function bumpNext(defaultBumpType) {
-    var manifest = manifests[i++];
+  function bumpNext (defaultBumpType) {
+    let manifest = manifests[i++];
     if (manifest) {
       return bumpManifest(manifest, defaultBumpType, options).then(bumpNext);
     }
@@ -99,11 +99,11 @@ function bumpManifests(manifests, options) {
  * @param {object} options - CLI options
  * @returns {Promise}
  */
-function bumpManifest(manifest, defaultBumpType, options) {
-  return new Promise(function(resolve, reject) {
+function bumpManifest (manifest, defaultBumpType, options) {
+  return new Promise((resolve, reject) => {
     if (options.prompt) {
       // Prompt the user for the type of bump to perform
-      var version = api.versionInfo(manifest, options);
+      let version = api.versionInfo(manifest, options);
       console.log('\nCurrent version in %s is %s', manifest, version.current);
 
       inquirer.prompt({
@@ -112,38 +112,39 @@ function bumpManifest(manifest, defaultBumpType, options) {
         message: 'How would you like to bump it?',
         default: defaultBumpType,
         choices: [
-          {value: 'major', name: 'major (' + version.nextMajor + ')'},
-          {value: 'minor', name: 'minor (' + version.nextMinor + ')'},
-          {value: 'patch', name: 'patch (' + version.nextPatch + ')'},
-          {value: 'premajor', name: 'pre-release major (' + version.nextPreMajor + ')'},
-          {value: 'preminor', name: 'pre-release minor (' + version.nextPreMinor + ')'},
-          {value: 'prepatch', name: 'pre-relase patch (' + version.nextPrePatch + ')'},
-          {value: 'prerelease', name: 'pre-release (' + version.nextPreRelease + ')'}
+          { value: 'major', name: 'major (' + version.nextMajor + ')' },
+          { value: 'minor', name: 'minor (' + version.nextMinor + ')' },
+          { value: 'patch', name: 'patch (' + version.nextPatch + ')' },
+          { value: 'premajor', name: 'pre-release major (' + version.nextPreMajor + ')' },
+          { value: 'preminor', name: 'pre-release minor (' + version.nextPreMinor + ')' },
+          { value: 'prepatch', name: 'pre-relase patch (' + version.nextPrePatch + ')' },
+          { value: 'prerelease', name: 'pre-release (' + version.nextPreRelease + ')' }
         ]
       })
-      .then(function(answer) {
+      .then((answer) => {
         api.runNpmScriptIfExists(manifest, 'preversion');
         bump(answer.bump);
       });
     }
     else {
-      var bumpType =
-            options.major ? 'major' :
-            options.minor ? 'minor' :
-            options.patch ? 'patch' :
-            options.premajor ? 'premajor' :
-            options.preminor ? 'preminor' :
-            options.prepatch ? 'prepatch' :
-            options.prerelease ? 'prerelease' :
-            defaultBumpType;
+      let bumpType =
+            options.major ? 'major'
+            : options.minor ? 'minor'
+            : options.patch ? 'patch'
+            : options.premajor ? 'premajor'
+            : options.preminor ? 'preminor'
+            : options.prepatch ? 'prepatch'
+            : options.prerelease ? 'prerelease'
+            : defaultBumpType;
       api.runNpmScriptIfExists(manifest, 'preversion');
       bump(bumpType);
     }
 
-    function bump(bumpType) {
+    function bump (bumpType) {
       try {
         api.bump(manifest, bumpType, options);
-      } catch(ex) {
+      }
+      catch (ex) {
         reject(ex);
       }
       resolve(bumpType);
