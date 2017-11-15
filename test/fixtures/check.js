363156428eec6866a05a5fe125dcaf38cef7337b
James Messinger <james.messinger@gmail.com>
2017-11-15 09:45:00 -0800

    refactored a test fixture, since Chalk removed the `chalk.stripColor()` method

--- test/fixtures/check.js
+++ test/fixtures/check.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const chalk = require('chalk');
+const stripAnsi = require('strip-ansi');
 const logSymbols = require('log-symbols');
 
 // Export the check symbol (✔ on Mac/Linux, √ on Windows)
 // with the ANSI color sequences removed
-module.exports = chalk.stripColor(logSymbols.success);
+module.exports = stripAnsi(logSymbols.success);
