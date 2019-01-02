02b51be143fdf26a12030198eb60cb0bc4bff5dc
James Messinger <james.messinger@gmail.com>
2019-01-02 06:58:09 -0600

    Corrected the `prereleaseTypes` and `releaseTypes`

--- src/release-type.ts
+++ src/release-type.ts
@@ -4,23 +4,25 @@ export { ReleaseType } from "semver";
 /**
  * The different types of pre-releases.
  */
-export const prereleaseTypes = ["preminor", "patch", "prepatch", "prerelease"];
+export const prereleaseTypes: ReleaseType[] = ["premajor", "preminor", "prepatch", "prerelease"];
 
 /**
  * All possible release types.
  */
-export const releaseTypes = prereleaseTypes.concat(["major", "premajor", "minor"]);
+export const releaseTypes: ReleaseType[] = prereleaseTypes.concat(["major", "minor", "patch"]);
+
+// tslint:disable: no-any no-unsafe-any
 
 /**
  * Determines whether the specified value is a pre-release.
  */
-export function isPrerelease(value: string): boolean {
+export function isPrerelease(value: any): boolean {
   return prereleaseTypes.includes(value);
 }
 
 /**
  * Determines whether the specified value is a valid ReleaseType string.
  */
-export function isReleaseType(value: string): value is ReleaseType {
+export function isReleaseType(value: any): value is ReleaseType {
   return releaseTypes.includes(value);
 }
