fba3d77c2509a9cf3e28d42cf577b758614f2de8
James Messinger <james.messinger@gmail.com>
2019-01-03 05:43:36 -0600

    Made the "clean .tmp directory" hook async

--- test/fixtures/clean-temp-dir.js
+++ test/fixtures/clean-temp-dir.js
@@ -6,10 +6,12 @@ const del = require("del");
 /**
  * Clean the .tmp directory before each test
  */
-beforeEach(() => {
+beforeEach("clean the .tmp directory", async () => {
   // Delete the .tmp directory, if it exists
-  del.sync("test/.tmp");
+  await del("test/.tmp");
 
   // Re-create the .tmp directory
-  fs.mkdirSync("test/.tmp");
+  await new Promise((resolve, reject) => {
+    fs.mkdir("test/.tmp", (err) => err ? reject(err) : resolve());
+  });
 });
