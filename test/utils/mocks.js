d7ed6223c9ad0601f62ae061fd212fc12d2f43b7
James Messinger <james.messinger@gmail.com>
2019-02-27 05:36:35 -0600

    Split the mocks into separate files for initial setup and testing

--- test/utils/mocks.js
+++ test/utils/mocks.js
@@ -1,20 +1,6 @@
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
 const files = require("./files");
-
-const mocksDir = path.resolve("test", "fixtures", "mocks");
-
-// Ensure that the mock binaries are executable
-fs.chmodSync(path.join(mocksDir, "git"), "0777");
-fs.chmodSync(path.join(mocksDir, "npm"), "0777");
-
-// Inject our mocks directory path into the PATH variable,
-// so that version-bump-prompt runs our mock `git` and `npm` binaries
-// instead of the real ones.
-let otherPaths = getEnvPath();
-process.env.PATH = mocksDir + path.delimiter + otherPaths; // eslint-disable-line no-path-concat
 
 const mocks = module.exports = {
   /**
@@ -69,20 +55,6 @@ const mocks = module.exports = {
     files.create("mocks.json", array);
   },
 };
-
-
-/**
- * Returns the PATH environment variable, case-insensitively
- */
-function getEnvPath () {
-  let keys = Object.keys(process.env);
-
-  for (let key of keys) {
-    if (key.toUpperCase() === "PATH") {
-      return process.env[key];
-    }
-  }
-}
 
 /**
  * Adds quotes around an argument if it contains whitespace characters
