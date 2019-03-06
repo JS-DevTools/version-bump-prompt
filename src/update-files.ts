de7f2bee3338a9e56bdeaa665e1601777fdcc05e
James Messinger <james.messinger@gmail.com>
2019-03-06 05:25:01 -0600

    Renamed the `name` variable to `relPath` to be more clear

--- src/update-files.ts
+++ src/update-files.ts
@@ -10,13 +10,13 @@ import { ProgressEvent } from "./version-bump-progress";
 export async function updateFiles(operation: Operation): Promise<Operation> {
   let { files } = operation.options;
 
-  for (let name of files) {
-    let modified = await updateFile(name, operation);
+  for (let relPath of files) {
+    let modified = await updateFile(relPath, operation);
 
     if (modified) {
       operation.update({
         event: ProgressEvent.FileUpdated,
-        files: [name],
+        files: [relPath],
       });
     }
   }
@@ -29,18 +29,18 @@ export async function updateFiles(operation: Operation): Promise<Operation> {
  *
  * @returns - `true` if the file was actually modified
  */
-async function updateFile(name: string, operation: Operation): Promise<boolean> {
-  name = path.basename(name).trim().toLowerCase();
+async function updateFile(relPath: string, operation: Operation): Promise<boolean> {
+  let name = path.basename(relPath).trim().toLowerCase();
 
   switch (name) {
     case "package.json":
     case "package-lock.json":
     case "bower.json":
     case "component.json":
-      return updateManifestFile(name, operation);
+      return updateManifestFile(relPath, operation);
 
     default:
-      return updateTextFile(name, operation);
+      return updateTextFile(relPath, operation);
   }
 }
 
@@ -52,19 +52,17 @@ async function updateFile(name: string, operation: Operation): Promise<boolean> 
  *
  * @returns - `true` if the file was actually modified
  */
-async function updateManifestFile(name: string, operation: Operation): Promise<boolean> {
+async function updateManifestFile(relPath: string, operation: Operation): Promise<boolean> {
   let { cwd } = operation.options;
   let { newVersion } = operation.state;
   let modified = false;
 
-    let file = await readJsonFile(name, cwd);
+  let file = await readJsonFile(relPath, cwd);
 
-    if (isManifest(file.data) && file.data.version !== newVersion) {
-      file.data.version = newVersion;
-      await writeJsonFile(file);
-      modified = true;
-    }
-  }
+  if (isManifest(file.data) && file.data.version !== newVersion) {
+    file.data.version = newVersion;
+    await writeJsonFile(file);
+    modified = true;
   }
 
   return modified;
@@ -75,27 +73,26 @@ async function updateManifestFile(name: string, operation: Operation): Promise<b
  *
  * @returns - `true` if the file was actually modified
  */
-async function updateTextFile(name: string, operation: Operation): Promise<boolean> {
+async function updateTextFile(relPath: string, operation: Operation): Promise<boolean> {
   let { cwd } = operation.options;
   let { oldVersion, newVersion } = operation.state;
   let modified = false;
 
-    let file = await readTextFile(name, cwd);
+  let file = await readTextFile(relPath, cwd);
 
-    // Only update the file if it contains at least one occurrence of the old version
-    if (file.data.includes(oldVersion)) {
-      // Escape all non-alphanumeric characters in the version
-      let sanitizedVersion = oldVersion.replace(/(\W)/g, "\\$1");
+  // Only update the file if it contains at least one occurrence of the old version
+  if (file.data.includes(oldVersion)) {
+    // Escape all non-alphanumeric characters in the version
+    let sanitizedVersion = oldVersion.replace(/(\W)/g, "\\$1");
 
-      // Replace occurrences of the old version number that are surrounded by word boundaries.
-      // This ensures that it matches "1.23.456" or "v1.23.456", but not "321.23.456".
-      let replacePattern = new RegExp("(\\b|v)" + sanitizedVersion + "\\b", "g");
+    // Replace occurrences of the old version number that are surrounded by word boundaries.
+    // This ensures that it matches "1.23.456" or "v1.23.456", but not "321.23.456".
+    let replacePattern = new RegExp("(\\b|v)" + sanitizedVersion + "\\b", "g");
 
-      file.data = file.data.replace(replacePattern, "$1" + newVersion);
-      await writeTextFile(file);
+    file.data = file.data.replace(replacePattern, "$1" + newVersion);
+    await writeTextFile(file);
 
-      return true;
-    }
+    return true;
   }
 
   return modified;
