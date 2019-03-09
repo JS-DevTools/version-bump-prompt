420ffe0cb93979611cd8dfda089be11841c98f6f
James Messinger <james.messinger@gmail.com>
2019-03-09 09:51:04 -0600

    Added additional file tests

--- test/specs/files.spec.js
+++ test/specs/files.spec.js
@@ -5,6 +5,42 @@ const { chaiExec, expect } = require("../utils/chai");
 
 describe.skip("bump [files...]", () => {
 
+  it("should update the package.json and package-lock.json by default", () => {
+    files.create("package.json", { version: "1.2.3" });
+    files.create("package-lock.json", { version: "1.2.3" });
+
+    let bump = chaiExec("major");
+
+    expect(bump).to.have.stderr("");
+    expect(bump).to.have.exitCode(0);
+
+    bump.should.have.stdout(
+      `${check} Updated package.json to 2.0.0\n` +
+      `${check} Updated package-lock.json to 2.0.0\n`
+    );
+
+    expect(files.json("package.json").version).to.equal("2.0.0");
+    expect(files.json("package-lock.json").version).to.equal("2.0.0");
+  });
+
+  it("should not update package.json if package-lock.json is explicitly specified", () => {
+    files.create("package.json", { version: "1.2.3" });
+    files.create("package-lock.json", { version: "1.2.3" });
+
+    let bump = chaiExec("major");
+
+    expect(bump).to.have.stderr("");
+    expect(bump).to.have.exitCode(0);
+
+    bump.should.have.stdout(
+      `${check} Updated package.json to 2.0.0\n` +
+      `${check} Updated package-lock.json to 2.0.0\n`
+    );
+
+    expect(files.json("package.json").version).to.equal("2.0.0");
+    expect(files.json("package-lock.json").version).to.equal("2.0.0");
+  });
+
   it("should replace the version number in non-manifest files", () => {
     files.create("package.json", { version: "1.2.3" });
     files.create("LICENSE", fixtures.license);
@@ -14,7 +50,7 @@ describe.skip("bump [files...]", () => {
     files.create("subdir/deep/script1.js", fixtures.script1);
     files.create("subdir/deep/script2.js", fixtures.script2);
 
-    let bump = chaiExec("major LICENSE README.* *.js");
+    let bump = chaiExec("major LICENSE README.* **/*.js");
 
     expect(bump).to.have.stderr("");
     expect(bump).to.have.exitCode(0);
@@ -33,8 +69,27 @@ describe.skip("bump [files...]", () => {
     expect(files.text("script1.js")).to.match(/let version = "2.0.0";/);
   });
 
+  it("should not replace other version fields in manifest files", () => {
+    files.create("package.json", { version: "1.2.3", notTheVersion: "1.2.3" });
+    files.create("package-lock.json", { version: "1.2.3", notTheVersion: "1.2.3" });
+
+    let bump = chaiExec("major");
+
+    expect(bump).to.have.stderr("");
+    expect(bump).to.have.exitCode(0);
+
+    bump.should.have.stdout(
+      `${check} Updated package.json to 2.0.0\n` +
+      `${check} Updated package-lock.json to 2.0.0\n`
+    );
+
+    expect(files.text("README.md")).to.match(/version 5.6.7 and v8.9.10 should not be changed/);
+    expect(files.text("script2.js")).to.match(/version 3.2.1 and v8.9.10 don't match the old version number/);
+  });
+
   it("should not replace other version numbers in non-manifest files", () => {
     files.create("package.json", { version: "1.2.3" });
+    files.create("package-lock.json", { version: "1.2.3" });
     files.create("LICENSE", fixtures.license);
     files.create("README.md", fixtures.readme);
     files.create("script1.js", fixtures.script1);
