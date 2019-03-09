d8e2f3cebd0e5c7ad4a274c03692d5369c3cc00e
James Messinger <james.messinger@gmail.com>
2019-03-09 08:37:37 -0600

    Removed debugging code

--- test/specs/cli.spec.js
+++ test/specs/cli.spec.js
@@ -13,20 +13,10 @@ describe("bump", () => {
     // Run the CLI without any arguments.
     // It will prompt the user and wait forever, so add a timeout.
     this.timeout(Math.max(10000, this.timeout()));
-    let bump = chaiExec("", { timeout: 5000 });
+    let bump = chaiExec("", { timeout: 6000 });
 
-    try {
-      // It should have prompted for input
-      expect(bump.stdout).to.contain("The current version in package.json is 1.0.0\nHow would you like to bump it? (Use arrow keys)");
-    }
-    catch (error) {
-      console.log(
-        "====================== begin bump output ======================\n",
-        bump,
-        "\n====================== end bump output ======================"
-      );
-      throw error;
-    }
+    // It should have prompted for input
+    expect(bump.stdout).to.contain("The current version in package.json is 1.0.0\nHow would you like to bump it? (Use arrow keys)");
   });
 
   it("should error if an invalid argument is used", () => {
