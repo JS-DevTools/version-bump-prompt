b7c9bd2271e9133a999199c85b68cfe12d3ec3fc
James Messinger <james.messinger@gmail.com>
2019-03-10 07:46:55 -0500

    Fleshed-out the mock script file to be more realistic

--- test/fixtures/files/script2.js
+++ test/fixtures/files/script2.js
@@ -1,4 +1,11 @@
-"use strict";
+/* eslint strict:off */
+(function () {
+  "use strict";
 
-// This file should not get updated because version 3.2.1
-// and v8.9.10 don't match the old version number
+  // This file should not get updated because
+  // version 3.2.1 and v8.9.10 don't match the old version number
+  let version = "3.2.1";
+  let someNumbers = "8.9.10";
+  return version + someNumbers;
+
+}());
