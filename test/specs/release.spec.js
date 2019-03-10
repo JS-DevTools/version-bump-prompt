5fd01a9d75393f872fd632e84eaaf51502143400
James Messinger <james.messinger@gmail.com>
2019-03-10 16:29:38 -0500

    Added tests for "bump #.#.#"

--- test/specs/release.spec.js
+++ test/specs/release.spec.js
@@ -3,96 +3,150 @@
 const { check, files, bump } = require("../utils");
 const { expect } = require("chai");
 
-describe.skip("bump [release]", () => {
+describe("bump [version]", () => {
 
-  it("should increment an all-zero version number", () => {
-    files.create("package.json", { version: "0.0.0" });
+  it("should accept an #.#.# version number", () => {
+    files.create("package.json", { version: "1.0.0" });
 
-    let cli = bump("major");
+    let cli = bump("123.45.678");
 
     expect(cli).to.have.stderr("");
     expect(cli).to.have.exitCode(0);
 
     expect(cli).to.have.stdout(
-      `${check} Updated package.json to 1.0.0\n`
+      `${check} Updated package.json to 123.45.678\n`
     );
 
-    expect(files.json("package.json")).to.deep.equal({ version: "1.0.0" });
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678" });
   });
 
-  it("should reset the minor and patch", () => {
-    files.create("package.json", { version: "1.2.3" });
+  it("should accept an #.#.# version number that's all zeroes", () => {
+    files.create("package.json", { version: "1.0.0" });
 
-    let cli = bump("--major");
+    let cli = bump("0.0.0");
 
     expect(cli).to.have.stderr("");
     expect(cli).to.have.exitCode(0);
 
     expect(cli).to.have.stdout(
-      `${check} Updated package.json to 2.0.0\n`
+      `${check} Updated package.json to 0.0.0\n`
     );
 
-    expect(files.json("package.json")).to.deep.equal({ version: "2.0.0" });
+    expect(files.json("package.json")).to.deep.equal({ version: "0.0.0" });
   });
 
-  it("should reset the prerelease version", () => {
-    files.create("package.json", { version: "1.2.3-beta.4" });
+  it("should accept an #.#.#-X version number", () => {
+    files.create("package.json", { version: "1.0.0" });
 
-    let cli = bump("--major");
+    let cli = bump("123.45.678-beta");
 
     expect(cli).to.have.stderr("");
     expect(cli).to.have.exitCode(0);
 
     expect(cli).to.have.stdout(
-      `${check} Updated package.json to 2.0.0\n`
+      `${check} Updated package.json to 123.45.678-beta\n`
     );
 
-    expect(files.json("package.json")).to.deep.equal({ version: "2.0.0" });
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678-beta" });
+  });
+
+  it("should accept an #.#.#-# version number", () => {
+    files.create("package.json", { version: "1.0.0" });
+
+    let cli = bump("123.45.678-910");
+
+    expect(cli).to.have.stderr("");
+    expect(cli).to.have.exitCode(0);
+
+    expect(cli).to.have.stdout(
+      `${check} Updated package.json to 123.45.678-910\n`
+    );
+
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678-910" });
+  });
+
+  it("should accept an #.#.#-X.# version number", () => {
+    files.create("package.json", { version: "1.0.0" });
+
+    let cli = bump("123.45.678-beta.910");
+
+    expect(cli).to.have.stderr("");
+    expect(cli).to.have.exitCode(0);
+
+    expect(cli).to.have.stdout(
+      `${check} Updated package.json to 123.45.678-beta.910\n`
+    );
+
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678-beta.910" });
+  });
+
+  it("should accept an #.#.#-#.# version number", () => {
+    files.create("package.json", { version: "1.0.0" });
+
+    let cli = bump("123.45.678-987.654");
+
+    expect(cli).to.have.stderr("");
+    expect(cli).to.have.exitCode(0);
+
+    expect(cli).to.have.stdout(
+      `${check} Updated package.json to 123.45.678-987.654\n`
+    );
+
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678-987.654" });
+  });
+
+  it("should accept an #.#.#-X.X version number", () => {
+    files.create("package.json", { version: "1.0.0" });
+
+    let cli = bump("123.45.678-alpha.beta");
+
+    expect(cli).to.have.stderr("");
+    expect(cli).to.have.exitCode(0);
+
+    expect(cli).to.have.stdout(
+      `${check} Updated package.json to 123.45.678-alpha.beta\n`
+    );
+
+    expect(files.json("package.json")).to.deep.equal({ version: "123.45.678-alpha.beta" });
   });
 
   it("should not be affected by the --preid flag", () => {
-    files.create("package.json", { version: "1.2.3-beta.4" });
+    files.create("package.json", { version: "1.0.0" });
 
-    let cli = bump("--major --preid alpha");
+    let cli = bump("1.2.3-beta.1 --preid alpha");
 
     expect(cli).to.have.stderr("");
     expect(cli).to.have.exitCode(0);
 
     expect(cli).to.have.stdout(
-      `${check} Updated package.json to 2.0.0\n`
+      `${check} Updated package.json to 1.2.3-beta.1\n`
     );
 
-    expect(files.json("package.json")).to.deep.equal({ version: "2.0.0" });
+    expect(files.json("package.json")).to.deep.equal({ version: "1.2.3-beta.1" });
   });
 
   it("should error if there's no current version number", () => {
-    files.create("package.json", {});
-
-    let cli = bump("major");
-
-    expect(cli).to.have.stdout("");
-    expect(cli).to.have.exitCode(2);
-
-    expect(cli).to.have.stderr(
-      "Unable to determine the current version number. Checked package.json, package-lock.json.\n"
-    );
-
-    expect(files.json("package.json")).to.deep.equal({});
-  });
-
-  it("should print a more detailed error if DEBUG is set", () => {
     files.create("package.json", { version: "" });
 
-    let cli = bump("major", { env: { DEBUG: "true" }});
+    let cli = bump("1.2.3");
 
     expect(cli).to.have.stdout("");
-    expect(cli).to.have.exitCode(2);
+    expect(cli).to.have.exitCode(1);
+    expect(cli).to.have.stderr("Unable to determine the current version number. Checked package.json.\n");
 
-    expect(cli).to.have.stderr.that.matches(
-      /^Error: Unable to determine the current version number. Checked package.json, package-lock.json.\n\s+at \w+/
-    );
+    expect(files.json("package.json")).to.deep.equal({ version: "" });
+  });
 
-    expect(files.json("package.json")).to.deep.equal({});
+  it("should error on an X.X.X version number", () => {
+    files.create("package.json", { version: "1.0.0" });
+
+    let cli = bump("A.B.C");
+
+    expect(cli).to.have.stdout("");
+    expect(cli).to.have.stderr("Could not find file: A.B.C.\n");
+    expect(cli).to.have.exitCode(1);
+
+    expect(files.json("package.json")).to.deep.equal({ version: "1.0.0" });
   });
 
 });
